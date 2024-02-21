ce5ff36fe2b208ab30f5373684593280a41aa308
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-21 16:58:42 +0100

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+TYPE = %w[Flat House Habitation Castle Condo Mansion Apartment Loft]
+
+puts "Cleaning the DB"
+Flat.destroy_all
+puts "DB cleaned"
+
+puts "Creating new flats..."
+15.times do
+  flat = Flat.new({
+    name: "#{TYPE.sample} #{Faker::Address.city}",
+    address: Faker::Address.full_address,
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    price_per_night: Faker::Number.between(from: 30, to: 1000),
+    number_of_guests: Faker::Number.between(from: 1, to: 16),
+    })
+  flat.save!
+  puts "#{flat.name} created"
+end
+puts "All new flats created"
